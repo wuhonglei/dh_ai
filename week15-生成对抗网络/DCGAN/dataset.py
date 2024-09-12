88f516f8b48cf2cf7630fb1ae08e801b433dc8a8
wuhonglei <1017368065@qq.com>
2024-09-12 23:07:21 +0800

    feat: 完成动漫数据的读取

---
+++ week15-生成对抗网络/DCGAN/dataset.py
@@ -0,0 +1,40 @@
+import os
+import torch.nn as nn
+from torch.utils.data import Dataset
+from torchvision import transforms
+from PIL import Image
+
+
+class AnimateDataset(Dataset):
+    def __init__(self, data_path, transform=None):
+        self.transform = transform
+        self.data = []
+        for image in os.listdir(data_path):
+            if image.endswith('.jpg') or image.endswith('.png'):
+                self.data.append(os.path.join(data_path, image))
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        sample = Image.open(self.data[idx])
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
+    transform = transforms.Compose([
+        transforms.Resize((64, 64)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    dataset = AnimateDataset('demo', transform)
+
+    # 显示图片
+    img = dataset[0]
+    img = img.permute(1, 2, 0).numpy()
+    plt.imshow(img)
+    plt.show()
