8b7821ba7c6e91911edba2e824bfa6688c17e8b3
Honglei Wu <honglei.wu@shopee.com>
2024-09-13 19:03:36 +0800

    feat: 优化损失代码计算

--- week16-神经风格迁移/3.内容损失与风格损失.py
+++ week16-神经风格迁移/3.内容损失与风格损失.py
@@ -1,52 +1,40 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
-from PIL import Image
-from torchvision import transforms
-import numpy as np
-import matplotlib.pyplot as plt
-
-from torchinfo import summary
 
 
-def load_image(image_path, max_size=400):
-    image = Image.open(image_path).convert('RGB')
-    size = min(max_size, max(image.size))
-    in_transform = transforms.Compose([
-        transforms.Resize((size, size)),
-        transforms.ToTensor(),
-        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-    ])
-    image = in_transform(image).unsqueeze(0)  # 为了符合模型输入的格式，增加一个维度
-    return image
-
-
-class VGG19(nn.Module):
-    """ 从VGG19中提取特征 """
-
+class VGGFeatures(nn.Module):
     def __init__(self):
-        super(VGG19, self).__init__()
+        super(VGGFeatures, self).__init__()
+        # 预训练的 VGG19 模型
         vgg_pretrained = models.vgg19(
             weights=models.VGG19_Weights.DEFAULT).features
 
-        self.styled_layers = ['0', '5', '10', '19', '28']  # 五个卷积层的首位索引
-        self.content_layer = '21'  # 21是VGG19中的第四个卷积层
+        # 定义需要提取的层
+        self.content_layers = ['21']  # conv4_2
+        # conv1_1, conv2_1, etc.
+        self.style_layers = ['0', '5', '10', '19', '28']
+
         self.model = nn.Sequential()
-        for name, layer in vgg_pretrained.named_children():
+        self.layers = self.content_layers + self.style_layers
+        self.layer_names = []  # 保存需要的层的名字(升序)
+        idx = 0
+
+        # 组合需要的层
+        for name, layer in vgg_pretrained._modules.items():
             self.model.add_module(name, layer)
+            if name in self.layers:
+                self.layer_names.append(name)
+            idx += 1
 
     def forward(self, x):
-        styles = []
-        content = None
-        for name, layer in self.model.named_children():
+        features = {}
+        for name, layer in self.model._modules.items():
             x = layer(x)
-            if name in self.styled_layers:
-                styles.append(x)
-            if name == self.content_layer:
-                content = x
-        return content, styles
+            if name in self.layer_names:
+                features[name] = x
+        return features
 
 
-def content_loss(content_feature, target):
-    # 计算内容损失和风格损失
-    return torch.mean((content_feature - target) ** 2)
+vgg = VGGFeatures()
+print(vgg.idx)
