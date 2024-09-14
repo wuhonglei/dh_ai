59f4f7b56c115a8c03146bfb2a43cf3d9fc411a2
Honglei Wu <honglei.wu@shopee.com>
2024-09-14 18:52:59 +0800

    feat: 参考视频纠正模型结构

--- week16-神经风格迁移/transformer_net.py
+++ week16-神经风格迁移/transformer_net.py
@@ -39,22 +39,25 @@ class TransformerNet(nn.Module):
         self.conv = nn.Sequential(
             # input: 3 x 256 x 256 output: 32 x 256 x 256
             nn.Sequential(
+                nn.ReflectionPad2d(padding=4),
                 nn.Conv2d(in_channels=channels, out_channels=32,
-                          kernel_size=9, stride=1, padding=4, bias=False),
+                          kernel_size=9, stride=1, bias=False),
                 nn.InstanceNorm2d(num_features=32, affine=True),
                 nn.ReLU()
             ),
             # input: 32 x 256 x 256 output: 64 x 128 x 128
             nn.Sequential(
+                nn.ReflectionPad2d(padding=1),
                 nn.Conv2d(in_channels=32, out_channels=64,
-                          kernel_size=3, stride=2, padding=1, bias=False),
+                          kernel_size=3, stride=2, bias=False),
                 nn.InstanceNorm2d(num_features=64, affine=True),
                 nn.ReLU()
             ),
             # input: 64 x 128 x 128 output: 128 x 64 x 64
             nn.Sequential(
+                nn.ReflectionPad2d(padding=1),
                 nn.Conv2d(in_channels=64, out_channels=128,
-                          kernel_size=3, stride=2, padding=1, bias=False),
+                          kernel_size=3, stride=2,  bias=False),
                 nn.InstanceNorm2d(num_features=128, affine=True),
                 nn.ReLU()
             ),
