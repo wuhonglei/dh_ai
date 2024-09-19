cd560cc1b94578c172479a1cd0111f984ea7b439
wuhonglei <1017368065@qq.com>
2024-09-19 23:11:42 +0800

    feat: 可视化 conv 层输出

--- 课后作业/vgg16/model.py
+++ 课后作业/vgg16/model.py
@@ -1,8 +1,12 @@
 """ 使用 vgg16 模型进行训练，识别 MNIST 手写数字 10 个类别 """
 
 import os
+import torch
 import torch.nn as nn
 from torchvision import models, transforms
+from PIL import Image
+import matplotlib.pyplot as plt
+
 
 from torchinfo import summary
 
@@ -38,6 +42,37 @@ transform = transforms.Compose([
 
 if __name__ == '__main__':
     model = VGG16()
-    print(model)
-    summary(model, input_size=(1, 3, 224, 224), col_names=(
-        "input_size", "output_size", "num_params"))
+    # summary(model, input_size=(1, 3, 224, 224), col_names=(
+    #     "input_size", "output_size", "num_params"))
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.load_state_dict(torch.load("./models/model_10.pth",
+                                     map_location=device, weights_only=True))
+
+    activation = {}
+
+    def get_activation(name):
+        def hook(model, input, output):
+            activation[name] = output.detach()  # 保存输出并分离梯度计算图
+        return hook
+
+    model.feature[4].register_forward_hook(get_activation('conv1'))
+    model.feature[9].register_forward_hook(get_activation('conv2'))
+    model.feature[16].register_forward_hook(get_activation('conv3'))
+    model.feature[23].register_forward_hook(get_activation('conv4'))
+    model.feature[30].register_forward_hook(get_activation('conv5'))
+
+    model.eval()
+    img = Image.open('./demo/dog.png')
+    img = transform(img).unsqueeze(0).to(device)
+    with torch.no_grad():
+        output = model(img)
+
+    fig, axes = plt.subplots(1, 5, figsize=(25, 5))
+    for i, (name, output) in enumerate(activation.items()):
+        single_img = output[0]
+        avg_img = single_img.mean(dim=0)
+        axes[i].imshow(avg_img, cmap='gray')
+        axes[i].set_title(name)
+
+    plt.show()
