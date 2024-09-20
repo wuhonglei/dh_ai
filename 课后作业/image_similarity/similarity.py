ff2a26a97553c14abae05d9dff9699d0ffafd11a
Honglei Wu <honglei.wu@shopee.com>
2024-09-20 10:25:09 +0800

    feat: 完成 vgg 图像特征提取

---
+++ 课后作业/image_similarity/similarity.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+from PIL import Image
+from torchvision import transforms
+
+from vgg16_feature import VGG16FeatureExtractor
+from vgg19_feature import VGG19FeatureExtractor
+
+
+def compare_similarity(model, img1, img2):
+    with torch.no_grad():
+        feature1 = model(img1)
+        feature2 = model(img2)
+
+    # Compute the similarity
+    cosine_sim = nn.CosineSimilarity(dim=1, eps=1e-6)
+    similarity = cosine_sim(feature1, feature2)
+    return similarity.item()
+
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+
+
+def process_img(img_path: str) -> torch.Tensor:
+    img = Image.open(img_path).convert('RGB')
+    img = transform(img).unsqueeze(0)  # type: ignore
+    return img
+
+
+if __name__ == '__main__':
+    # model = VGG16FeatureExtractor()
+    model = VGG19FeatureExtractor()
+    model.eval()
+
+    img1 = process_img('data/dog/3.png')
+    img2 = process_img('data/dog/4.png')
+
+    similarity = compare_similarity(model, img1, img2)
+    print(f'Similarity: {similarity}')
---
+++ 课后作业/image_similarity/vgg16_feature.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from torchinfo import summary
+
+
+class VGG16FeatureExtractor(nn.Module):
+    def __init__(self):
+        super(VGG16FeatureExtractor, self).__init__()
+        vgg16 = models.vgg16(weights=models.VGG16_Weights.DEFAULT)
+        self.features = vgg16.features
+        self.avgpool = vgg16.avgpool
+        self.classifier = nn.Sequential(*list(vgg16.classifier)[:-1])
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
+if __name__ == '__main__':
+    model = VGG16FeatureExtractor()
+    summary(model, input_size=(1, 3, 224, 224))
