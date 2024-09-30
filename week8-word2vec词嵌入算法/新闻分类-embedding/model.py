d9063733e1363719c85f0f85e7f0e520abdc6ad1
Honglei Wu <honglei.wu@shopee.com>
2024-09-30 18:34:06 +0800

    feat: 打印模型参数

--- week8-word2vec词嵌入算法/新闻分类-embedding/model.py
+++ week8-word2vec词嵌入算法/新闻分类-embedding/model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torchinfo import summary
 
 
 class TextClassifier(nn.Module):
@@ -19,3 +20,9 @@ class TextClassifier(nn.Module):
         x = torch.sum(x, dim=1)  # x: [batch_size, embedding_dim]
         x = self.classifier(x)
         return x
+
+
+if __name__ == '__main__':
+    model = TextClassifier(55592, 128, 20, 0)
+    summary(model, input_data=torch.randint(0, 55592, (4, 85)),
+            device='cpu', col_names=("input_size", "output_size", "num_params"))
