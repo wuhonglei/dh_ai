b5266154a174e6e8b4896200c008cf6284182954
Honglei Wu <honglei.wu@shopee.com>
2024-10-18 14:22:02 +0800

    feat: 创建项目初始框架

--- 课后作业/元素匹配/dom-detect/src/index.tsx
+++ 课后作业/元素匹配/dom-detect/src/index.tsx
@@ -1,9 +1,1 @@
-/* @refresh reload */
-import { render } from 'solid-js/web'
-
-import './index.css'
-import App from './App'
-
-const root = document.getElementById('root')
-
-render(() => <App />, root!)
+console.info("index.tsx");
