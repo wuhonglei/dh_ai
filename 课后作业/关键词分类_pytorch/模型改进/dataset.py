831456d82f5a4cd10aa83ef38edad72699a57d2d
wuhonglei <1017368065@qq.com>
2024-10-21 01:04:07 +0800

    feat: 自定义用户词典

--- 课后作业/关键词分类_pytorch/模型改进/dataset.py
+++ 课后作业/关键词分类_pytorch/模型改进/dataset.py
@@ -129,7 +129,7 @@ def tokenize_th(text: str) -> list[str]:
 def tokenize_tw(text: str) -> list[str]:
     stop_word_list = set(stopwords.words(
         'english') + stopwords.words('chinese') +
-        get_stop_words('./stopwords_custom.txt'))
+        get_stop_words('./stopwords_custom.txt') + ['220v', '110v'])
 
     """
     + 符号在 keyword 中表示语义或者连接关系，需要替换为空格, 例如 sharp+microwave -> sharp microwave
@@ -155,32 +155,17 @@ def tokenize_tw(text: str) -> list[str]:
 
     keyword = re.sub(r'[;\'"?!]$', ' ', keyword)
 
-    """
-    英文单词和中文之间增加空格，例如 三星samsung -> 三星 samsung
-    jieba 分词支持拆分中英文，例如 三星samsung -> ['三星', 'samsung']
-    """
-    # keyword = re.sub(
-    #     r'([a-zA-Z\d]+)(?=[\u4e00-\u9fa5])|([\u4e00-\u9fa5]+)(?=[a-zA-Z\d])', r'\1\2 ', keyword)
-
-    """
-    电压标识符号和中文之间增加空格，例如 220v冰箱 -> 220v 冰箱
-    jieba 分词支持拆分中英文，例如 220v冰箱 -> ['220v', '冰箱']
-    """
-    # keyword = re.sub(
-    #     r'(220v|110v)(?=[\u4e00-\u9fa5]+)|([\u4e00-\u9fa5]+)(?=220v|110v)', r'\1\2 ', keyword, flags=re.IGNORECASE)
-
     def parse_num(match):
         num = re.sub(r'(g|G|T|t)', '', match.group(1))
         if num.isdigit() and int(num) % 2 == 0:
-            return match.group(1) + ' '
+            return ' '
         else:
             return match.group()
     """
-    数字和中文之间增加空格，例如 2g内存 -> 2g 内存
-    jieba 分词支持拆分中英文，例如 2g内存 -> ['2g', '内存']
+    存储单位 32g内存条 中 32g 是容量修饰词，内存是实体词, 无需保留单位
     """
-    # keyword = re.sub(
-    #     r'\b(\d+(?:g|G|T|t))(?=[\u4e00-\u9fa5]+)', parse_num, keyword)
+    keyword = re.sub(
+        r'\b(\d+(?:g|G|T|t))(?=[\u4e00-\u9fa5]|\s)', parse_num, keyword)
 
     jieba.load_userdict('./userdict/tw.txt')
     token_list = jieba.lcut(keyword)
