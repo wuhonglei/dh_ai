0c1497b68fb28e847d78a1325fab9cfa70b1dd85
wuhonglei <1017368065@qq.com>
2024-10-21 02:15:22 +0000

    feat: 更新模型依赖

--- 课后作业/关键词分类/Keyword-Categories/main.py
+++ 课后作业/关键词分类/Keyword-Categories/main.py
@@ -28,6 +28,8 @@ class KeywordCategories:
         # raw_data = get_gsheet_df("Result-" + self.country)
         raw_data = get_df_from_xlsx(
             './data/Keyword_Categorization.xlsx', self.country)
+        # 去重
+        raw_data = raw_data.drop_duplicates(subset=['Keyword'], keep='first')
 
         # Step2 > Pre-process data
         process_data = pre_process(raw_data, self.stopwords)
