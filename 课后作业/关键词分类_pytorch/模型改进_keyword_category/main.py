fff938278e53e4aedc6b16ab57e27d983e093947
wuhonglei <1017368065@qq.com>
2024-10-22 23:54:51 +0800

    feat: 新建文件夹

---
+++ 课后作业/关键词分类_pytorch/模型改进_keyword_category/main.py
@@ -0,0 +1,40 @@
+from sklearn.model_selection import train_test_split
+from dataset import get_data
+from train import train
+import time
+
+countries_info = [
+    {"country": "SG", "stopwords": "english"},
+    {"country": "MY", "stopwords": "english"},
+    {"country": "TH", "stopwords": "english"},
+    {"country": "TW", "stopwords": "chinese"},
+    {"country": "VN", "stopwords": "english"},
+    {"country": "ID", "stopwords": "english"},
+    {"country": "PH", "stopwords": "english"},
+    {"country": "BR", "stopwords": "spanish"},
+    {"country": "MX", "stopwords": "spanish"},
+    {"country": "CL", "stopwords": "spanish"},
+    {"country": "CO", "stopwords": "spanish"},
+]
+
+data_list: list[dict] = []
+start_time = time.time()
+excel = get_data('./data/Keyword Categorization.xlsx')
+print(f"Time taken to load data: {time.time() - start_time:.2f}s")
+for info in countries_info:
+    country = info["country"]
+    if country != "SG":
+        continue
+    data = excel[country].drop_duplicates(
+        subset=['Keyword'], keep='first').reset_index(drop=True)  # type: ignore
+    X = data["Keyword"]
+    y = data["Category"]
+    # 使用 train_test_split 将数据划分为训练集和测试集
+    X_train, X_test, y_train, y_test = train_test_split(
+        X.tolist(), y.tolist(), test_size=0.05, random_state=0)
+
+    train(X_train, y_train, country, X_test, y_test)
+
+
+# df = pd.DataFrame(data_list)
+# df.to_csv("./csv/模型复现.csv", index=False)
---
+++ 课后作业/关键词分类_pytorch/模型改进_keyword_category/tokennizer/utils.py
@@ -0,0 +1,71 @@
+import os
+import re
+
+
+def get_current_dir() -> str:
+    return os.path.dirname(os.path.abspath(__file__))
+
+
+def get_abs_path(file_path: str) -> str:
+    return os.path.join(get_current_dir(), file_path)
+
+
+def get_stop_words(file_path: str) -> list[str]:
+    with open(get_abs_path(file_path), "r") as file:
+        return file.read().split()
+
+
+def add_space_between_unit(text: str, units: list[str]) -> str:
+    """
+    在 text 的单位之间添加空格
+    :param text: 输入文本
+    :param units: 单位列表
+    :return: 添加空格后的文本
+    @example:
+    >>> add_space_between_unit("1kg", "1 kg ")
+    """
+    for unit in units:
+        # 先处理 单位左侧含有数字的情况
+        pattern = r"(?<=\d)\s*" + f'({re.escape(unit)})' + r"(?=([^a-zA-Z]|$))"
+        text = re.sub(pattern, r' \1', text, re.IGNORECASE)
+
+        # 再处理 单位右侧含有名词的情况
+        pattern = r"(?<=\b)" + f'({re.escape(unit)})' + r"(?=[^a-zA-Z])"
+        text = re.sub(pattern, r'\1 ', text, re.IGNORECASE)
+
+    return text
+
+
+def is_chinese_text(text: str) -> bool:
+    """
+    判断 text 是否为中文文本
+    """
+    return bool(re.search(r'^[\u4e00-\u9fa5]+$', text))
+
+
+def contain_chinese_text(text: str) -> bool:
+    """
+    判断 text 是否包含中文文本
+    """
+    return bool(re.search(r'[\u4e00-\u9fa5]+', text))
+
+
+def is_one_ascii(text: str) -> bool:
+    """
+    判断 text 是否为单个 ascii 字符
+    """
+    return bool(re.match(r'^[a-zA-Z\d]$', text))
+
+
+def is_number(text: str) -> bool:
+    """
+    判断 text 是否为数字
+    """
+    return bool(re.match(r'^\d+$', text))
+
+
+def is_float(text: str) -> bool:
+    """
+    判断 text 是否为浮点数
+    """
+    return bool(re.match(r'^\d+\.\d+$', text))
