f8baa988a2aceb85e954a56d06ada282e178f933
wuhonglei <1017368065@qq.com>
2024-10-22 10:03:36 +0000

    feat: 使用 spacy 进行英文分词

--- 课后作业/关键词分类_pytorch/模型改进/tokennizer/sg.py
+++ 课后作业/关键词分类_pytorch/模型改进/tokennizer/sg.py
@@ -1,12 +1,13 @@
 import re
-import nltk
-from nltk.corpus import stopwords
+import spacy
 from .utils import get_stop_words
+
+nlp = spacy.load('en_core_web_sm')
 
 
 def tokenize_sg(text: str) -> list[str]:
-    stop_word_list = set(stopwords.words(
-        'english') + get_stop_words('./stopwords/common.txt'))
+    stop_word_list = set(list(nlp.Defaults.stop_words) +
+                         get_stop_words('./stopwords/common.txt'))
 
     """
     + 符号在 keyword 中表示语义或者连接关系，需要替换为空格, 例如 sharp+microwave -> sharp microwave
@@ -29,7 +30,11 @@ def tokenize_sg(text: str) -> list[str]:
     / 符号在数字中间，仅用于表示尺寸，因此需要移除左右两边的内容, 例如 3/4 pants mens -> pants mens
     经过验证，该处理在 one-hot svm 不会提升准确率，因此不需要处理
     """
-    # keyword = re.sub(r'\d+/\d+', '', keyword)
+    keyword = re.sub(r'\d+/\d+', '', keyword)
+
+    units = ['inch']
+    for unit in units:
+        keyword = re.sub(r'(?<=\d)' + unit + r'(?=\w+)', '', keyword)
 
     token_list = nltk.word_tokenize(keyword, language='english')
     new_token_list = []
