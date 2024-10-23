384d5093f0fb3dede43edf8089283e64e8492ee6
Honglei Wu <honglei.wu@shopee.com>
2024-10-23 16:00:25 +0800

    feat: 使用 keyword_filter 进行过滤

--- 课后作业/关键词分类/Keyword-Categories/main.py
+++ 课后作业/关键词分类/Keyword-Categories/main.py
@@ -27,10 +27,10 @@ class KeywordCategories:
         # Step1 > Get gsheet raw data
         # raw_data = get_gsheet_df("Result-" + self.country)
         raw_data = get_df_from_csv(
-            f'./data/{self.country.lower()}.csv')
+            f'./data/result/{self.country.lower()}.csv')
         # 去重
-        raw_data = raw_data.drop_duplicates(
-            subset=['Keyword'], keep='first').reset_index(drop=True)
+        # raw_data = raw_data.drop_duplicates(
+        #     subset=['Keyword'], keep='first').reset_index(drop=True)
 
         # Step2 > Pre-process data
         process_data = pre_process(raw_data, self.stopwords)
