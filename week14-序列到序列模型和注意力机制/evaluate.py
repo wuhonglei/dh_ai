a00a2324d76183cfcd54e1399bd554fb525ad4ff
wuhonglei <1017368065@qq.com>
2024-11-04 15:15:39 +0000

    feat: 完成文本翻译训练

--- week14-序列到序列模型和注意力机制/evaluate.py
+++ week14-序列到序列模型和注意力机制/evaluate.py
@@ -8,23 +8,19 @@ from torch.utils.data import DataLoader
 train_dataset = TranslateDataset('./csv/train.csv')
 test_dataset = TranslateDataset('./csv/test.csv')
 
-
-def collate(batch): return collate_fn(batch, src_vocab, target_vocab)
-
-
 src_vocab, target_vocab = build_vocab(train_dataset)
-test_loader = DataLoader(test_dataset, batch_size=32,
-                         shuffle=False, collate_fn=collate)
+test_loader = DataLoader(test_dataset, batch_size=1,
+                         shuffle=False)
 
 
 input_size = len(src_vocab)  # 词典大小
 output_size = len(target_vocab)  # 词典大小
-embed_size = 50  # 词向量维度
-hidden_size = 100  # 隐藏层维度
-num_layers = 1  # LSTM层数
+embed_size = 256  # 词向量维度
+hidden_size = 512  # 隐藏层维度
+num_layers = 2  # LSTM层数
 dropout = 0.5  # dropout概率
 teacher_force_ratio = 0.5
-num_epochs = 3
+num_epochs = 5
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 encoder = Encoder(input_size, embed_size, hidden_size,
@@ -32,13 +28,13 @@ encoder = Encoder(input_size, embed_size, hidden_size,
 decoder = Decoder(output_size, embed_size, hidden_size,
                   num_layers, dropout).to(device)
 model = Seq2Seq(encoder, decoder, device).to(device)
-model.load_state_dict(torch.load('./models/seq2seq_0.pth'))
+model.load_state_dict(torch.load('./models/seq2seq_1.pth'))
 
 model.eval()
 for i, (src, target) in enumerate(test_loader):
-    src = src.to(device)
-    target = target.to(device)
     # 对于每个测试样本，调用函数test_sample测试效果
+    src = [token[0] for token in src]
+    target = [token[0] for token in target]
     predict, target, bleu_score = test_samples(
         model, src, target, src_vocab, target_vocab)
     # 打印测试结果
