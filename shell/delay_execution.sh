f56446b72b34383e90c53748593c2f4ace8a7616
wuhonglei <1017368065@qq.com>
2024-11-13 16:22:57 +0000

    feat: 增加自动关机的脚本

---
+++ shell/delay_execution.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 延迟 2 小时 (7200 秒)
+sleep 7200
+
+# 执行指定的代码
+export $(cat /proc/1/environ | tr '\0' '\n' | grep MATCLOUD_CANCELTOKEN)&&/public/script/matncli node cancel -url https://matpool.com/api/public/node
---
+++ shell/readme.md
@@ -0,0 +1,5 @@
+## 查找进程
+
+```bash
+ps aux | grep delay_execution.sh
+```
