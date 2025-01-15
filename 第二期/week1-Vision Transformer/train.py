5634d262984b76079c1be12f702c7b45fc627122
wuhonglei <1017368065@qq.com>
2025-01-15 15:07:46 +0000

    style: 代码格式化

--- 第二期/week1-Vision Transformer/train.py
+++ 第二期/week1-Vision Transformer/train.py
@@ -17,20 +17,25 @@ config = wandb.config
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225]),
 ])
 
 # 加载 CIFAR-10 数据集
-train_dataset = datasets.CIFAR10(root='/mnt/dataset', train=True, download=True, transform=transform)
-test_dataset = datasets.CIFAR10(root='/mnt/dataset', train=False, download=True, transform=transform)
+train_dataset = datasets.CIFAR10(
+    root='/mnt/dataset', train=True, download=True, transform=transform)
+test_dataset = datasets.CIFAR10(
+    root='/mnt/dataset', train=False, download=True, transform=transform)
 
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
 # 定义模型
-model = VisionTransformer(img_size=224, patch_size=16, in_channels=3, n_classes=10, embed_dim=768, depth=12, n_heads=12, mlp_ratio=4., qkv_bias=True, drop_rate=0., attn_drop_rate=0.)
+model = VisionTransformer(img_size=224, patch_size=16, in_channels=3, n_classes=10, embed_dim=768,
+                          depth=12, n_heads=12, mlp_ratio=4., qkv_bias=True, drop_rate=0., attn_drop_rate=0.)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
+
 
 def test(model, test_loader):
     model.eval()
@@ -77,9 +82,11 @@ def train(model, train_loader, epochs):
             'test_accuracy': test_accuracy,
             'epoch_time': int(time.time() - start_time)
         })
-        epoch_progress.set_postfix(avg_loss=avg_loss, accuracy=f"{100 * test_accuracy:.2f}%")
+        epoch_progress.set_postfix(
+            avg_loss=avg_loss, accuracy=f"{100 * test_accuracy:.2f}%")
 
     print("Training complete")
+
 
 def clean_up():
     # 保存模型权重
