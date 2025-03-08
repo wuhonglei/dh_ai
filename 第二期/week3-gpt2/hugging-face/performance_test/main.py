e7646533010a6e2ff747ae6f9a25451883de86b3
wuhonglei <1017368065@qq.com>
2025-03-08 15:18:51 +0800

    feat: 完善模型评估代码

--- 第二期/week3-gpt2/hugging-face/performance_test/main.py
+++ 第二期/week3-gpt2/hugging-face/performance_test/main.py
@@ -1,11 +1,15 @@
 import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 from evaluate import load
-
-from utils.common import get_device
+from tqdm import tqdm
+from utils.common import get_device, get_text_token_len, get_max_token_len, write_to_file
 from dataset import WritingPromptsDataset
+import torchvision
+from pprint import pprint
 
-model_name = "gpt2-xl"  # 可替换为 "gpt2-medium", "gpt2-large" 等
+torchvision.disable_beta_transforms_warning()
+
+model_name = "gpt2-large"  # 可替换为 "gpt2-medium", "gpt2-large" 等
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 model = GPT2LMHeadModel.from_pretrained(model_name)
 
@@ -32,25 +36,24 @@ validation_dataset = WritingPromptsDataset(
 def generate_text(model, prompt, max_length=100):
     inputs = tokenizer(prompt, return_tensors="pt",
                        max_length=1024, truncation=True).to(device)
-    outputs = model.generate(
-        **inputs,
-        do_sample=False,
-        max_length=max_length,
-        num_return_sequences=1,
-        no_repeat_ngram_size=3,  # 避免重复
-        top_p=0.95,  # 使用 nucleus sampling
-        temperature=0.7,  # 控制随机性
-        pad_token_id=tokenizer.eos_token_id  # 避免警告
-    )
-    generated = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    # 移除提示部分，仅保留生成内容
-    return generated.replace(prompt, "").strip()
+    try:
+        outputs = model.generate(
+            **inputs,
+            do_sample=False,
+            max_length=max_length,
+            num_return_sequences=1,
+            no_repeat_ngram_size=3,  # 避免重复
+            # top_p=0.95,  # 使用 nucleus sampling
+            # temperature=0.7,  # 控制随机性
+            pad_token_id=tokenizer.eos_token_id  # 避免警告
+        )
+        generated = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        # 移除提示部分，仅保留生成内容
+        return generated.replace(prompt, "").strip()
+    except Exception as e:
+        print(f"Error: {e}")
+        return ""
 
-
-# 测试生成
-prompt = "The sun began to set over the calm ocean."
-generated = generate_text(model, prompt)
-print("Generated Text:", generated)
 
 # 加载评估指标
 bleu = load("bleu")
@@ -60,17 +63,29 @@ rouge = load("rouge")
 predictions = []
 references = []
 
-for example in test_dataset:
+progress = tqdm(test_dataset, desc="Generating Text")  # type: ignore
+for example in progress:
     prompt = example["prompt"]
     reference = example["story"]
-    generated = generate_text(model, prompt)
+    prompt_len = get_text_token_len(prompt)
+    story_len = get_text_token_len(reference)
+    max_len = get_max_token_len(prompt_len, story_len)
+    generated = generate_text(model, prompt, max_len)
     predictions.append(generated)
     references.append(reference)
+    break
 
 
 # 计算 BLEU 和 ROUGE
 bleu_score = bleu.compute(predictions=predictions, references=references)
 rouge_score = rouge.compute(predictions=predictions, references=references)
 
-print(f"BLEU Score: {bleu_score['bleu']:.4f}")  # type: ignore
-print(f"ROUGE Scores: {rouge_score}")
+data = {
+    "model_name": model_name,
+    'dataset': 'test',
+    'max_length': 'min(max(prompt_len+story_len, min_len), max_len)',
+    "bleu_score": bleu_score['bleu'],  # type: ignore
+    "rouge_score": rouge_score
+}
+pprint(data)
+write_to_file(f"./performance.txt", data)
--- 第二期/week3-gpt2/hugging-face/performance_test/utils/common.py
+++ 第二期/week3-gpt2/hugging-face/performance_test/utils/common.py
@@ -1,13 +1,24 @@
 import torch
 from typing import Literal
+import json
 
 
 def get_device() -> Literal["mps", "cuda", "cpu"]:
-    # if torch.backends.mps.is_available():  # type: ignore
-    #     return 'mps'
-    # elif torch.cuda.is_available():
-    #     return 'cuda'
-    # else:
-    #     return 'cpu'
+    if torch.cuda.is_available():
+        return 'cuda'
+    else:
+        return 'cpu'
 
-    return 'cpu'
+
+def get_text_token_len(text: str) -> int:
+    return len(text.split())
+
+
+def get_max_token_len(prompt_len: int, story_len: int, min_len: int = 50, max_len: int = 900) -> int:
+    return min(max(prompt_len+story_len, min_len), max_len)
+
+
+def write_to_file(file_path: str, content: dict):
+    # 追加写入
+    with open(file_path, 'a') as f:
+        f.write(json.dumps(content) + '\n')
