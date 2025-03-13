7f132cc6c5d4ffa0527901275538cde8bca989ba
wuhonglei <1017368065@qq.com>
2025-03-13 04:15:41 +0000

    feat: 保存代码修改

--- 第二期/week3-gpt2/hugging-face/performance_test/eval.py
+++ 第二期/week3-gpt2/hugging-face/performance_test/eval.py
@@ -3,6 +3,14 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 from datasets import Dataset
 from transformers import Trainer, TrainingArguments
 from typing import TypedDict
+import torch.distributed as dist
+import json
+import os
+
+
+def is_distributed_env():
+    local_rank = int(os.environ.get("LOCAL_RANK", -1))
+    return local_rank != -1
 
 
 class ExampleDict(TypedDict):
@@ -52,8 +60,8 @@ def build_dataset(prompt_path: str, story_path: str, tokenizer: GPT2Tokenizer) -
     stories = Dataset.from_text(story_path)
 
     dataset = Dataset.from_dict({
-        'prompt': prompts['text'][:100],  # type: ignore
-        'story': stories['text'][:100]  # type: ignore
+        'prompt': prompts['text'][:10],  # type: ignore
+        'story': stories['text'][:10]  # type: ignore
     })
     dataset = dataset.map(
         lambda example: preprocess_function(tokenizer, example),
@@ -63,13 +71,22 @@ def build_dataset(prompt_path: str, story_path: str, tokenizer: GPT2Tokenizer) -
     return dataset
 
 
+def write_to_file(text: str, file_path: str):
+    with open(file_path, 'a') as f:
+        f.write(text + '\n')
+
+
 def main():
+    is_distributed = is_distributed_env()
+
     model_name = 'gpt2'
     tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
     tokenizer.pad_token = tokenizer.eos_token
 
     model = GPT2LMHeadModel.from_pretrained(model_name)
+    print(model.config)
+
     test_dataset = build_dataset(
         './writingPrompts/test.wp_source',
         './writingPrompts/test.wp_target',
@@ -81,6 +98,8 @@ def main():
         output_dir='./results',
         eval_strategy='epoch',
         learning_rate=2e-5,
+        local_rank=-1,
+        ddp_backend="nccl" if is_distributed else None,  # 使用NCCL作为分布式后端
         per_device_eval_batch_size=4,
     )
 
@@ -90,8 +109,16 @@ def main():
         eval_dataset=test_dataset,
     )
 
-    perplexity = trainer.evaluate()
-    print(perplexity)
+    performance = trainer.evaluate()
+    if training_args.local_rank == 0:
+        data = {
+            'desc': 'multi process' if is_distributed else 'single process',
+            'performance': performance,
+        }
+        # write_to_file(json.dumps(data), './eval.txt')
+
+    if is_distributed:
+        dist.destroy_process_group()
 
 
 if __name__ == '__main__':
