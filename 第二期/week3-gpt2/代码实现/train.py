34655e7bb35a8b216a8d5ff3fa88ae34d25733a9
Honglei Wu <honglei.wu@shopee.com>
2025-03-17 14:02:15 +0800

    feat: 增加可测试性

--- 第二期/week3-gpt2/代码实现/train.py
+++ 第二期/week3-gpt2/代码实现/train.py
@@ -21,16 +21,20 @@ def train():
         'vocab_size_or_config_json_file': len(encoder),
         'n_positions': 1024,
         'n_ctx': 1024,
-        'n_embd': 128,
+        'n_embd': 32,
         'n_layer': 2,
-        'n_head': 4,
+        'n_head': 2,
         'layer_norm_epsilon': 1e-5,
     })
     model = GPT2LMHeadModel(config)
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    epoch_progress = tqdm(range(100), desc="Epoch")
+    epoch_progress = tqdm(range(150), desc="Epoch")
+
+    prompt = input('请输入prompt:')
     for epoch in epoch_progress:
+        epoch_progress.write(''.join(encoder.decode(sample_sequence(
+            model, 25, context=encoder.encode(jieba.lcut(prompt)), device=device, sample=False, top_k=10)[0].tolist())))
         for input_ids, labels in dataloader:
             input_ids = input_ids.to(device)
             labels = labels.to(device)
@@ -40,15 +44,6 @@ def train():
             loss.backward()
             optimizer.step()
         epoch_progress.set_postfix(loss=loss.item())
-
-    while True:
-        prompt = input('请输入prompt:')
-        if prompt == 'q':
-            break
-
-        context = encoder.encode(jieba.lcut(prompt))
-        print(''.join(encoder.decode(sample_sequence(
-            model, 25, context=context, device=device, sample=False, top_k=10)[0].tolist())))
 
 
 if __name__ == "__main__":
