851aa7854d7fe776df1a0e619a0c945aae95825c
wuhonglei <1017368065@qq.com>
2025-03-25 14:31:25 +0800

    feat: 保存新的 test 测试数据

--- ai_interview/projects/text-similarity/bert/utils/sample_val_csv.py
+++ ai_interview/projects/text-similarity/bert/utils/sample_val_csv.py
@@ -10,10 +10,10 @@ import pandas as pd
 sys.path.append('../')
 # fmt: on
 
-src_csv_path = '../data/val_10000.csv'
+src_csv_path = '../data/test_10000.csv'
 src_csv_name = os.path.basename(src_csv_path).split('.')[0]
 df = pd.read_csv(src_csv_path)
 
 batch_size = 1000
 df.sample(batch_size, random_state=42).to_csv(
-    f'../data/evaluate/标题测试原材料_{batch_size}_from_{src_csv_name}.csv'.format(batch_size, src_csv_name), index=False)
+    f'../data/标题测试原材料_{batch_size}_from_{src_csv_name}.csv', index=False)
