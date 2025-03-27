d2c7100c804ea29c1477eb91ae055521afc31e46
wuhonglei <1017368065@qq.com>
2025-03-27 16:50:38 +0800

    feat: 保存 RNN 模型测试结果

--- ai_interview/projects/text-similarity/word2vector-siamese/model.py
+++ ai_interview/projects/text-similarity/word2vector-siamese/model.py
@@ -12,11 +12,11 @@ class SiameseNetwork(nn.Module):
         self.pad_idx = pad_idx
         self.embedding = nn.Embedding(
             vocab_size, embedding_dim, padding_idx=pad_idx)
-        self.lstm = nn.LSTM(embedding_dim,
-                            hidden_size=hidden_dim,
-                            batch_first=True,
-                            bidirectional=False)
-        self.projection = nn.Linear(hidden_dim, projection_dim)
+        self.rnn = nn.RNN(embedding_dim,
+                          hidden_size=hidden_dim,
+                          batch_first=True,
+                          bidirectional=False)
+        # self.projection = nn.Linear(hidden_dim, projection_dim)
 
     def load_pretrained_embedding_model(self, path: str):
         if not os.path.exists(path):
@@ -29,9 +29,9 @@ class SiameseNetwork(nn.Module):
         # [batch_size, seq_len] -> [batch_size, seq_len, embedding_dim]
         embedded = self.embedding(input_ids)
         # 输出形状 [batch_size, seq_len, hidden_dim]
-        output, (hidden, _) = self.lstm(embedded)
+        output, hidden = self.rnn(embedded)
         new_hidden = hidden.squeeze(0)
-        return self.projection(new_hidden)
+        return new_hidden
 
     def forward_pair(self, input_ids_1: torch.Tensor, input_ids_2: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         output_1 = self.forward(input_ids_1)
