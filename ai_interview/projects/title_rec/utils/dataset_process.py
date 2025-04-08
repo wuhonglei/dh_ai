bef26b80e12368779eea40f62bd47e907f35bfd9
wuhonglei <1017368065@qq.com>
2025-04-08 14:51:12 +0800

    feat: 保存基线模型的效果

--- ai_interview/projects/title_rec/utils/dataset_process.py
+++ ai_interview/projects/title_rec/utils/dataset_process.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import glob
+import os
+from tqdm import tqdm
+from config import dataset_dir, clean_dataset_dir
 
 
 def remove_columns():
@@ -22,5 +26,24 @@ def remove_columns():
         df.to_csv(save_path, index=False)
 
 
+def join_csv():
+    csv_dir = dataset_dir
+    output_dir = clean_dataset_dir
+    input_csv_files = glob.glob(os.path.join(csv_dir, '*.csv'))
+    columns = ['main_image', 'attached_images',
+               'download_success', 'main_image_name']
+    for csv_file in tqdm(input_csv_files, desc='join csv'):
+        csv_name = os.path.basename(csv_file)
+        src_df = pd.read_csv(csv_file)
+        dest_df = pd.read_csv(os.path.join(output_dir, csv_name))
+        src_df.set_index('item_id', inplace=True)
+        dest_df.set_index('item_id', inplace=True)
+        for index, row in tqdm(dest_df.iterrows(), desc=f'join csv {csv_name}', total=len(dest_df)):
+            if index in src_df.index:
+                for column in columns:
+                    dest_df.loc[index, column] = src_df.loc[index, column]
+        dest_df.to_csv(os.path.join(output_dir, csv_name), index=True)
+
+
 if __name__ == "__main__":
-    remove_columns()
+    join_csv()
