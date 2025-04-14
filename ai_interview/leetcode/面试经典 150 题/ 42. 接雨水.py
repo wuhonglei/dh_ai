9cc46d2db6778470c209db5f69f951b6cb998ba1
wuhonglei <1017368065@qq.com>
2025-04-14 23:14:09 +0800

    feat: 完成接雨水算法题

--- ai_interview/leetcode/面试经典 150 题/ 42. 接雨水.py
+++ ai_interview/leetcode/面试经典 150 题/ 42. 接雨水.py
@@ -7,20 +7,17 @@ class Solution:
             return 0
         
         left_max = [0] * n
-        right_max = [0] * n
         left_max[0] = height[0]
-        right_max[n-1] = height[n-1]
 
         # 从左往右遍历
         for i in range(1, n):
             left_max[i] = max(left_max[i-1], height[i])
         
-        for j in range(n-2, -1, -1):
-            right_max[j] = max(right_max[j+1], height[j])
-
+        right_max = height[n-1]
         ans = 0
-        for i in range(n):
-            ans += min(left_max[i], right_max[i]) - height[i]
+        for j in range(n-2, -1, -1):
+            right_max = max(right_max, height[j])
+            ans += min(right_max, left_max[j]) - height[j]
         
         return ans
 
