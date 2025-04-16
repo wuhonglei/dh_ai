205653d7b329c0fe8886caa3537af205e637428f
wuhonglei <1017368065@qq.com>
2025-04-16 18:58:10 +0800

    feat: 支持 batch_classier

--- ai_interview/projects/title_rec/models/llama_model/zero_shot/config.py
+++ ai_interview/projects/title_rec/models/llama_model/zero_shot/config.py
@@ -10,6 +10,7 @@ columns = [
     'remove_spacy_stop_words'
 ]
 model_path = '../Llama-3.2-1B'
+name_col = 'name'
 label_id_col = 'level1_global_be_category_id'
 project_name = 'shopee_title_llama_zero_shot_model'
 label_names_csv_path = './label_names.csv'
--- ai_interview/projects/title_rec/models/llama_model/zero_shot/eval.py
+++ ai_interview/projects/title_rec/models/llama_model/zero_shot/eval.py
@@ -1,8 +1,10 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 import json
-from config import test_csv_path, label_id_col, tree_json_path, model_path, label_names_csv_path
+from config import test_csv_path, name_col, label_id_col, tree_json_path, model_path, label_names_csv_path
 import pandas as pd
 from tqdm import tqdm
+import torch
+from torch.utils.data import Dataset, DataLoader
 
 
 model_name = model_path
@@ -14,13 +16,14 @@ classifier = pipeline("text-generation", model=model,
                       tokenizer=tokenizer, max_new_tokens=5)
 
 
-def classify(text: str, labels: str) -> str:
-    prompt = f'Text: "{text}"\nQuestion: Which category does this text belong to? {labels}\nAnswer:'
-    result: str = classifier(prompt)[0]['generated_text']  # type: ignore
+def batch_classify(texts: list[str], categories: str) -> list[str]:
+    prompts = [f'Text: "{text}"\nQuestion: Which category does this text belong to? {categories}\nAnswer:'
+               for text in texts]
+    results: list[str] = classifier(prompts)  # type: ignore
     # 提取模型输出的类别，可以用正则或简单字符串处理
-    predict_name = result.split(
-        "Answer:")[-1].strip().split('\n')[0].strip()
-    return predict_name
+    predict_names = [result.split(
+        "Answer:")[-1].strip().split('\n')[0].strip() for result in results]
+    return predict_names
 
 
 def get_label_map(csv_path: str) -> tuple[dict, dict, list]:
@@ -36,19 +39,38 @@ def get_label_map(csv_path: str) -> tuple[dict, dict, list]:
 id_to_name, name_to_id, label_names = get_label_map(label_names_csv_path)
 
 
-test_df = pd.read_csv(test_csv_path)
+class TextDataset(Dataset):
+    def __init__(self, csv_path: str, id_to_name: dict[int, str]):
+        self.df = pd.read_csv(csv_path)
+        self.texts = self.df[name_col].tolist()
+        self.label_ids = self.df[label_id_col].tolist()
+        self.id_to_name = id_to_name
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx) -> tuple[str, str]:
+        text = self.texts[idx]
+        label_id = self.label_ids[idx]
+        label_name = self.id_to_name[label_id]
+        return text, label_name
+
+
+dataset = TextDataset(csv_path=test_csv_path, id_to_name=id_to_name)
+dataloader = DataLoader(dataset, batch_size=8, shuffle=False)
+
 correct = 0
-total = len(test_df)
 temp_total = 0
-progress_bar = tqdm(test_df.iterrows(), total=len(test_df))
-for index, row in progress_bar:
-    text = row['name']
-    label_id = row[label_id_col]
-    label_name = id_to_name[label_id]
-    predict_name = classify(text, ', '.join(label_names))
-    temp_total += 1
-    if label_name.lower() == predict_name.lower():
-        correct += 1
+total = len(dataset)
+progress_bar = tqdm(dataloader, total=len(dataloader))
+for batch in progress_bar:
+    batch_texts: list[str] = batch["text"]
+    batch_labels: list[str] = batch["label_name"]
+    predict_names = batch_classify(batch_texts, ', '.join(label_names))
+    temp_total += len(batch_texts)
+    for label, predict in zip(batch_labels, predict_names):
+        if label.lower() == predict.lower():
+            correct += 1
 
     # 每隔 10% 打印一次进度
     if temp_total % (total // 10) == 0:
