d367af997eae8cc8721418ae4a3baa00871c718d
wuhonglei <1017368065@qq.com>
2025-04-16 21:58:31 +0800

    feat: 完成整数转罗马数字运算

--- ai_interview/leetcode/面试经典 150 题/12. 整数转罗马数字.py
+++ ai_interview/leetcode/面试经典 150 题/12. 整数转罗马数字.py
@@ -1,25 +1,31 @@
 class Solution:
-    char_map = {
-        4: [(1000, 'M')], # 千位
-        3: [(900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C')], # 百位
-        2: [(90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X')], # 十位
-        1: [(9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
-    }
+    value_symbols = [
+        (1000, 'M'),
+        (900, 'CM'),
+        (500, 'D'),
+        (400, 'CD'),
+        (100, 'C'), # 百位
+        (90, 'XC'),
+        (50, 'L'),
+        (40, 'XL'),
+        (10, 'X'), # 十位
+        (9, 'IX'),
+        (5, 'V'), 
+        (4, 'IV'), 
+        (1, 'I')
+    ]
+
     def intToRoman(self, num: int) -> str:
-        n = len(str(num))
         ans = ''
-        # 从最高位开始遍历
-        while n > 0:
-            char_list = Solution.char_map[n]
-            for base, c in char_list:
-                count = num // base
-                if count > 0:
-                    ans += c * count
-                    num = num % base
-                    if num == 0:
-                        return ans
-            n -= 1
+        for value, symbol in Solution.value_symbols:
+            while num >= value:
+                num -= value
+                ans += symbol
+            if num == 0:
+                break
         return ans
+
+
 
 case_list = [
     (3749, "MMMDCCXLIX"),
