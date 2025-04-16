197953c5024d18e0e811aaad77ec7aff1b1edaeb
wuhonglei <1017368065@qq.com>
2025-04-16 16:50:30 +0800

    feat: 调整为 torchvision  的 b7 预训练模型结构

--- ai_interview/projects/title_rec/models/image_pretrained/model.py
+++ ai_interview/projects/title_rec/models/image_pretrained/model.py
@@ -6,19 +6,16 @@ import torch
 import torch.nn as nn
 from torchinfo import summary
 import timm
+from torchvision.models import efficientnet_b7, EfficientNet_B7_Weights, EfficientNet
 
 
 class ImageModel(nn.Module):
     def __init__(self, num_classes: int, model_name: str, drop_rate: float):
         super(ImageModel, self).__init__()
-        self.image_model = timm.create_model(
-            model_name,
-            pretrained=True,
-            num_classes=num_classes,
-            in_chans=3,
-            drop_rate=drop_rate,
-            global_pool='avg'
-        )
+        self.image_model = efficientnet_b7(
+            weights=EfficientNet_B7_Weights.DEFAULT)
+        self.image_model.classifier[-1] = nn.Linear(
+            self.image_model.classifier[-1].in_features, num_classes)  # type: ignore
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.image_model(x)
@@ -26,7 +23,8 @@ class ImageModel(nn.Module):
 
 
 if __name__ == '__main__':
-    model = ImageModel(num_classes=10, model_name='vgg16', drop_rate=0.5)
+    model = ImageModel(
+        num_classes=10, model_name='efficientnet_b7', drop_rate=0.5)
     print(model)
-    summary(model, input_size=(1, 3, 224, 224), col_names=[
+    summary(model, input_size=(1, 3, 600, 600), col_names=[
             "input_size", "output_size", "num_params", "params_percent"])
