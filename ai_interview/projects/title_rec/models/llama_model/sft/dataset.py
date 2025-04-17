33e7b5dc878702c1e200498d453df54d578136f4
wuhonglei <1017368065@qq.com>
2025-04-17 10:10:13 +0800

    feat: 实现了 sft 基本代码

---
+++ ai_interview/projects/title_rec/models/llama_model/sft/dataset.py
@@ -0,0 +1,28 @@
+from torch.utils.data import Dataset
+from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast
+import pandas as pd
+
+
+class TitleRecDataset(Dataset):
+    def __init__(self, csv_path: str, tokenizer: PreTrainedTokenizer | PreTrainedTokenizerFast, name_col: str, label_id_col: str, id_to_name: dict[int, str], label_names: list[str]):
+        self.csv_path = csv_path
+        self.df = pd.read_csv(csv_path)
+        self.tokenizer = tokenizer
+        self.name_col = name_col
+        self.label_id_col = label_id_col
+        self.id_to_name = id_to_name
+        self.label_names = label_names
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+        name = row[self.name_col]
+        label_id = row[self.label_id_col]
+        label_name = self.id_to_name[label_id]
+        prompt = f'Text: "{name}"\nWhich category? [{", ".join(self.label_names)}]\nAnswer:'
+        return {
+            "input_ids": self.tokenizer(prompt, truncation=True, padding="max_length", max_length=128)["input_ids"],
+            "labels": self.tokenizer(label_name, truncation=True, padding="max_length", max_length=10)["input_ids"]
+        }
---
+++ ai_interview/projects/title_rec/models/llama_model/sft/train.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer, PreTrainedTokenizer, PreTrainedTokenizerFast
+from torch.utils.data import DataLoader
+
+from config import train_csv_path, test_csv_path, name_col, label_id_col, label_names_csv_path
+from dataset import TitleRecDataset
+
+
+def get_label_map(csv_path: str) -> tuple[dict, dict, list]:
+    df = pd.read_csv(csv_path)
+    name_col = 'label_names'
+    id_col = 'label_ids'
+    label_names = df[name_col].unique().tolist()
+    id_to_name = {row[id_col]: row[name_col] for _, row in df.iterrows()}
+    name_to_id = {row[name_col]: row[id_col] for _, row in df.iterrows()}
+    return id_to_name, name_to_id, label_names
+
+
+def build_dataloader(csv_path: str, tokenizer: PreTrainedTokenizer | PreTrainedTokenizerFast, name_col: str, label_id_col: str, id_to_name: dict[int, str], label_names: list[str]):
+    dataset = TitleRecDataset(
+        csv_path, tokenizer, name_col, label_id_col, id_to_name, label_names)
+    return DataLoader(dataset, batch_size=8, shuffle=True)
+
+
+model_name = "meta-llama/Llama-3.2-1B"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+id_to_name, name_to_id, label_names = get_label_map(label_names_csv_path)
+train_dataloader = build_dataloader(
+    train_csv_path, tokenizer, name_col, label_id_col, id_to_name, label_names)
+test_dataloader = build_dataloader(
+    test_csv_path, tokenizer, name_col, label_id_col, id_to_name, label_names)
+
+
+training_args = TrainingArguments(
+    per_device_train_batch_size=8,
+    num_train_epochs=3,
+    output_dir="./llama3-classifier"
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataloader.dataset,
+    eval_dataset=test_dataloader.dataset
+)
+
+trainer.train()
